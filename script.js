2bff52658eea6b75e13ed6baaa40317ca4da5f26
JampOreo <joaquinmedinapedrozo@gmail.com>
2025-08-19 08:12:50 -0300

    wololooo

---
+++ script.js
@@ -0,0 +1,4 @@
+// Este script es muy simple, solo muestra una alerta al hacer clic en un post.
+function showPostAlert() {
+    alert("¡Hiciste clic en un post! En un blog real, esto te llevaría a la página completa del post. 😉");
+}
